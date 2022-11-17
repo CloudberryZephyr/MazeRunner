d52115da10ed3fb3584ccf5f13e09a06541f8d3a
CloudberryZephyr <ShoemakerCM21@gcc.edu>
2022-11-17 15:43:25 -0500

    Completed Player class minus a few questions: 1) what does the player initially have? 2) where does the player start? 3) does the Player object persist throughout the game, or is there a new Player object for every level? 4) how does the player communicate what direction they wish to move?

--- src/Player.java
+++ src/Player.java
@@ -1,19 +1,78 @@
 import java.util.ArrayList;
 
 public class Player {
-    public Player(){}
     private ArrayList<String> backPack;
+    private int health;
     private Location location;
-    public void move(){}
 
+    /**
+     * player constructor method
+     * TODO: determine what to give the player initially
+     * TODO: determine where to start the player
+     * TODO: is there one player throughout the game, or a new player object for each level?
+     */
+    public Player(){
+        backPack = new ArrayList<>();
+        health = 50; // set initial health to 50, we'll see if this is practical or not
+        location = new Location(0, 0); // starts the player in the top left corner by default
+    }
 
-    private int Health;
-    public void loseHealth(){}
-    public void gainHealth(){}
-    public int getHealth(){}
+    /**
+     * moves the Player by changing its location
+     * @param direction string representation of the four movement options: up, down, left, and right
+     *                  TODO: what key words are we going to use?
+     */
+    public void move(String direction){
+        if (direction.equals("up") && location.getY()>0) {
+            location.setLocation(location.getX(), location.getY()-1);
+        }
+        if (direction.equals("down") && location.getY()</* some measure for max board y */) {
+            location.setLocation(location.getX(), location.getY()+1);
+        }
+        if (direction.equals("left") && location.getX()>0) {
+            location.setLocation(location.getX()-1, location.getY());
+        }
+        if (direction.equals("right") && location.getX()</* some measure for max board x */) {
+            location.setLocation(location.getX()+1, location.getY());
+        }
+    }
 
-    private boolean hasKey(){}
+    /**
+     * Method for losing a given amount of health
+     * @param amt int amount of health to lose
+     */
+    public void loseHealth(int amt){
+        health -= amt;
+    }
 
+    /**
+     * Method for gaining a given amount of health
+     * @param amt int amount of health to gain
+     */
+    public void gainHealth(int amt){
+        health += amt;
+    }
+
+    /**
+     * getter for int data member health
+     * @return int value of health variable
+     */
+    public int getHealth(){
+        return health;
+    }
+
+    /**
+     * method for determining if the player has picked up the key in the past
+     * @return boolean value - true if the key has been picked up, false if it has not
+     */
+    private boolean hasKey(){
+        return backPack.contains("key");
+    }
+
+    /**
+     * getter method for the player's current location
+     * @return Location object for the player - NOTE: this is the same object, not a copy
+     */
     public Location getLocation(){
         return location;
     }

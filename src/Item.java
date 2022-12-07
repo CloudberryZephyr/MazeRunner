58c4a90c8cf6ef5c98afaa4aab5faa8151e92e1c
MUNCYMM20 <Muncymm20@gcc.edu>
2022-12-07 11:41:56 -0500

    added more I-O into Key as well as curCell method

--- src/Item.java
+++ src/Item.java
@@ -1,9 +1,13 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public abstract class Item {
     private Player p1;
     private int maxHealth;
     protected int blockAmt;
     protected int incomingDamage;
     protected int playerDamage;
+    Scanner scan = new Scanner(System.in);
 
 
     public Item (Player player,int blockAmt, int incomingDamage){
@@ -20,6 +24,16 @@ public abstract class Item {
         p1.loseHealth(playerDamage,"fallDamage");
     }
 
+    public void useItem(){
+        System.out.println("would you like to use an item?");
+        String itemYN = scan.nextLine().toLowerCase(Locale.ROOT);
+        if(itemYN.equals("y")){
+            System.out.println("which item would you like to use?");
+            System.out.println(p1.getBackPack());
+            int itemCH = scan.nextInt();
+
+        }
+    }
     public int getCurHealth(){
         return p1.getHealth();
     }

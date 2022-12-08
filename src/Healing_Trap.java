516cd49bbe64f93dc9df73b075e2722f41a473a8
MUNCYMM20 <Muncymm20@gcc.edu>
2022-12-07 20:43:19 -0500

    fixed shield so that it actually blocks incoming damage

--- src/Healing_Trap.java
+++ src/Healing_Trap.java
@@ -3,9 +3,10 @@ public class Healing_Trap extends Cell{
 
     public Healing_Trap(Player player){
         this.p1 = player;
+        refillHealth();
     }
 
-    public void refillHealth(int rateOfHealing){
+    public void refillHealth(){
         // want to change this so that the player is healed at an amount per second.
         // And movement is blocked while healing is taking place.
         System.out.println("Congratulation you found a healing room, this room will refill your health to full");
--- src/Shield.java
+++ src/Shield.java
@@ -6,8 +6,21 @@ public class Shield extends Item{
     public Shield(Player player, int blockAmt, int incomingDamage){
         super(player, blockAmt, incomingDamage);
         this.name = "Shield";
-        this.blockAmt = 15;
+        this.blockAmt = 5;
         this.incomingDamage = incomingDamage;
+        loseHealthAfterBlock();
+    }
+
+    public int takeDamage() {
+        String type = "f";
+
+        p1.loseHealth(incomingDamage,type);
+        return p1.getFallHealth();
+    }
+
+    public void loseHealthAfterBlock(){
+        playerDamage = incomingDamage - blockAmt;
+        p1.loseHealth(playerDamage,"f");
     }
 
 
